12fd12709b554bd1a9f3d2699dab5283879bf1ee
GAGmanga <gatolindo@naver.com>
2024-10-02 11:47:36 +0900

    #5_reducer BookList의 동작을 reducer를 사용하여 구현하도록 refactoring 함. reducer.js 새로 작성.

--- src/component/bookables/reducer.js
+++ src/component/bookables/reducer.js
@@ -33,7 +33,6 @@ export default function reducer(state, action){
         case "PREV_BOOKABLE":
         {
             // const count = state.bookables.filter(b=> b.group === state.group).length;
-
             return{
                 ...state,
                 bookableIndex: (state.bookableIndex === 0) ? action.payload-1 : (state.bookableIndex-1) % action.payload
