0dedbde5ebce2f12df510c06c93fc3c629137e95
GAGmanga <gatolindo@naver.com>
2024-10-07 15:14:29 +0900

    #11 state 공유하기: useContext App.jsx에 context 선언

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,54 @@ import UserPicker from "./component/Users/UserPicker.jsx";
 import BookingsPage from "./component/Bookings/BookingsPage.jsx";
 import BookablePage from "./component/Bookables/BookablePage.jsx";
 import UsersPage from "./component/Users/UsersPage.jsx";
+import UserContext from "./component/users/UserContext.js";
+import {useState} from "react";
 
 function App() {
 
+    const [user, setUser] = useState()
+    console.log('-App user -', user)
+
   return (
-    <BrowserRouter>
-        <div className="App">
-            <header>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/bookings" className="btn btn-header">
-                                <FaCalendarAlt/>
-                                <span>예약</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/bookables" className="btn btn-header">
-                                <FaDoorOpen/>
-                                <span>예약자원</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/users" className="btn btn-header">
-                                <FaUsers/>
-                                <span>사용자</span>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <UserPicker/>
-            </header>
+      <UserContext.Provider value={{user, setUser}}>
+        <BrowserRouter>
+            <div className="App">
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/bookings" className="btn btn-header">
+                                    <FaCalendarAlt/>
+                                    <span>예약</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/bookables" className="btn btn-header">
+                                    <FaDoorOpen/>
+                                    <span>예약자원</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/users" className="btn btn-header">
+                                    <FaUsers/>
+                                    <span>사용자</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <UserPicker/>
+                </header>
 
-        <Routes>
-            <Route path="/bookings" element={<BookingsPage/>}/>
-            <Route path="/bookables" element={<BookablePage/>}/>
-            <Route path="/users" element={<UsersPage/>}/>
-        </Routes>
+            <Routes>
+                <Route path="/bookings" element={<BookingsPage/>}/>
+                <Route path="/bookables" element={<BookablePage/>}/>
+                <Route path="/users" element={<UsersPage/>}/>
+            </Routes>
 
-        </div>
-    </BrowserRouter>
-    /* 사용자 선택한 메뉴 항목에 따라 화면에 보이는 UI 를 결정합니다.*/
-
+            </div>
+        </BrowserRouter>
+        {/* 사용자 선택한 메뉴 항목에 따라 화면에 보이는 UI 를 결정합니다.*/}
+      </UserContext.Provider>
   )
 }
 
