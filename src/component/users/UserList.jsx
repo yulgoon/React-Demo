a658b7905dbb4b2a7a66a2c5a388d439d6abe856
GAGmanga <gatolindo@naver.com>
2024-10-07 15:05:26 +0900

    #10 Users 기본 기능 userDetails 분리

--- src/component/users/UserList.jsx
+++ src/component/users/UserList.jsx
@@ -1,14 +1,20 @@
 import {useEffect, useState} from "react";
 import PageSpinner from "../UI/PageSpinner.jsx";
 
-function UserList (){
+function UserList({user, setUser}) {
     const [users, setUsers] = useState(null)
     // fetch 중 오류 또는 로딩 중에 상태값
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
-    const [userIndex, setUserIndex] = useState(0)
-    const user = users?.[userIndex]  //자바의 Optional 역할 연산자 ?.
+    
+/*  index를 사용하지 않으므로써 필요 없어진 코드
+    // index를 사용하면 event handle가 아닌 곳에서 상태 변경 함수를 실행
+    // 따라서 index를 사용하지 않도록 refactoring합니다.
+    // const [userIndex, setUserIndex] = useState(0)
+    // const user = users?.[userIndex]  //자바의 Optional 역할 연산자 ?.
     //users 가 null 이 아닐 때만 실행합니다.
+    // 선택된 인덱스 값으로 user를 바꾸어야 합니다. - 부모, 형제 컴포넌트와 공유할 수 있는 방법으로 바꿉니다. (user의 state를 변경)
+    // setUser(user?.[userIndex])*/
 
     //api 서비스 제공하는 서버로부터 데이터 가져오기
     useEffect(() => {
@@ -40,17 +46,16 @@ function UserList (){
     return(
         <>
             {users && (<ul className="users items-list-nav">
-                {users.map((u, i) => (
-                    <li key={u.id}
-                        className={i === userIndex ? "selected" : null}>
-                        <button className="btn btn-header"
-                                onClick={() => setUserIndex(i)}>
+                {users.map((u) => (
+                    <li key={u.id} className={(u.id === user?.id) ? "selected" : null}>
+                        <button className="btn btn-header" onClick={() => setUser(u)}>
                             {u.name}
                         </button>
                     </li>
                 ))}
             </ul>)}
-            {user && (<div className="item user">
+            {/*UserDetails로 컨포넌트 분리합니다.*/}
+{/*            {user && (<div className="item user">
                 <div className="item-header">
                     <h2>{user.name}</h2>
                 </div>
@@ -58,7 +63,7 @@ function UserList (){
                     <h3>{user.title}</h3>
                     <p>{user.notes}</p>
                 </div>
-            </div>)}
+            </div>)}*/}
         </>
     )
 }
--- src/component/users/UsersPage.jsx
+++ src/component/users/UsersPage.jsx
@@ -1,5 +1,5 @@
 import UsersList from "./UserList.jsx";
-import {useContext, useState} from "react";
+import {useState} from "react";
 import UserDetails from "./UserDetails.jsx";
 
 
@@ -9,6 +9,7 @@ function UsersPage () {
 
     return (
         <main className="users-page">
+            {/*선택한 user를 형제 컨포넌트와 공유하기 위해 부모 컴포넌트 UsersPatge가 user 상태를 선언했으므로 UserList도 props를 선언합니다.*/}
             <UsersList user={user} setUser={setUser}/>
             <UserDetails user={user}/>
         </main>
